faf204232901f4cb3c70fa7bd1590c5d885b4768
NavSrijan <nav.srijan.official@gmail.com>
2026-06-01 11:48:38 +0530

    integrated all services in main

--- main.py
+++ main.py
@@ -1,35 +1,62 @@
 import os
 import asyncio
+import uvicorn
+import logging
 from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
-from llm.connect import LLM
+from llm.connect_v2 import LLM
+from llm.worker import llm_worker
+
 from comm.telegram_bot import TelegramBot, run_bot
+from comm.server import app
 
-load_dotenv()  # Load environment variables from .env file
+from db.connection import db
 
-
-bot = TelegramBot(token=os.getenv("TELEGRAM_BOT_TOKEN"))
-
-def main():
-    print("Hello from sevak!")
-    config = {"configurable": {"session_id": "nav"}}
-    llm = LLM()
-    chain = llm.get_chain()
-
-    x = ""
-    while x != "exit":
-        x = input("You: ")
-        if x != "exit":
-            response = chain.invoke({"input": x}, config=config)
-            print(f"Ram Prakash: {response.content}")
+from utils.setup_logging import setup_logging
 
 
-@bot.on_message()
-async def greet(update, context):
-    await update.message.reply_text(f"Hello, {update.effective_user.first_name}!")
+logger = logging.getLogger(__name__)
+
+async def main():
+
+    load_dotenv()  # Load environment variables from .env file
+    setup_logging()  # Set up logging configuration
+    input_queue = asyncio.Queue()
+
+    await db.connect() # Database
+    
+    engine = create_async_engine(os.getenv("PG_DATABASE_URL"))
+    AsyncSessionLocal = sessionmaker(
+            engine, class_=AsyncSession, expire_on_commit=False
+        )
+
+    llm = await LLM.create()
+
+    bot = TelegramBot(token=os.getenv("TELEGRAM_BOT_TOKEN"))
+    bot_data = {}
+    bot_data['input_queue'] = input_queue
+
+    app.state.input_queue = input_queue
+    config = uvicorn.Config(app, host="localhost", port=6969, log_level="warning")
+    server = uvicorn.Server(config)
+
+
+    try:
+        await asyncio.gather(
+                server.serve(),
+                run_bot(bot, bot_data),
+                llm_worker(input_queue, llm)
+            )
+    finally:
+        await llm.aclose()
+        await db.disconnect()
+
 
 
 if __name__ == "__main__":
-    # main()
-    asyncio.run(run_bot(bot))
-
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info("Exiting gracefully...")
